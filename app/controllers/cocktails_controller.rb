acf1ca889743d6f7eeaa052e5657c02635b67379
alexandre cousin <cousin.a@laposte.net>
2019-02-15 17:26:34 +0100

    end of day two

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,6 +1,6 @@
 class CocktailsController < ApplicationController
   def index
-    @cocktails = Cocktail.all
+    @cocktails = Cocktail.all.order("name ASC")
   end
 
   def show
@@ -19,9 +19,18 @@ class CocktailsController < ApplicationController
     redirect_to cocktails_path
   end
 
+  def update
+    @cocktail = Cocktail.find(params[:id])
+    if @cocktail.update(cocktail_params)
+      redirect_to cocktail_path(@cocktail)
+    else
+      render update
+    end
+  end
+
   private
 
   def cocktail_params
-    params.require(:cocktail).permit(:name)
+    params.require(:cocktail).permit(:name, :photo)
   end
 end
